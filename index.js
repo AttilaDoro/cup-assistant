b5ce0fda2518bd67b700243e2e4ef615be03e483
AttilaDoro <doroattila@gmail.com>
2018-04-06 19:29:37 +0200

    fix express route

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = express()
 const path = require('path')
 
 app.use(express.static('dist'))
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './dist/index.html')))
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, './dist/index.html')))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
