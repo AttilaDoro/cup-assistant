0d0e7a892136b0205423d0a239547c65853cd550
AttilaDoro <doroattila@gmail.com>
2018-04-14 17:35:02 +0200

    remove logs

--- src/reducers/points.js
+++ src/reducers/points.js
@@ -17,18 +17,14 @@ const initialState = {
 const points = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_POINTS_FOR_KILL_VALUE:
-      console.log({ ...state, pointForKill: action.killValue })
       return { ...state, pointForKill: action.killValue }
     case REMOVE_POINT_FOR_POSITION_ROW:
-      console.log({ ...state, pointsForPositions: state.pointsForPositions.filter((pos, index) => index !== action.posIndex) })
       return { ...state, pointsForPositions: state.pointsForPositions.filter((pos, index) => index !== action.posIndex) }
       case UPDATE_POINT_FOR_POSITION_VALUE:
       const newPointsForPositions = [...state.pointsForPositions]
       newPointsForPositions[action.posIndex].killValue = action.killValue
-      console.log({ ...state, pointsForPositions: newPointsForPositions })
       return { ...state, pointsForPositions: newPointsForPositions }
     case ADD_NEW_POINT_FOR_POSITION_ROW:
-      console.log({ ...state, pointsForPositions: [...state.pointsForPositions, { killValue: 0 }] })
       return { ...state, pointsForPositions: [...state.pointsForPositions, { killValue: 0 }] }
     default:
       return state
