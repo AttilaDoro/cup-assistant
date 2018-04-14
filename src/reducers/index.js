7ec4cfb3271f3e51d991616c0f75a0f424f6f7fa
AttilaDoro <doroattila@gmail.com>
2018-04-14 02:18:49 +0200

    clean up

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,6 @@
 import { combineReducers } from 'redux'
-import todos from './todos'
-import visibilityFilter from './visibilityFilter'
 import matches from './matches'
  
 export default combineReducers({
-  todos,
-  visibilityFilter,
   matches,
 })
